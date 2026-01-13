1c4b227726ec375f64fc5bb8e58b487cc50269c2
ramble6120-lang <ramble6120@gmail.com>
2026-01-13 15:01:17 +0900

    配線修正

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './TripApp.jsx'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'  // ← 必ず './App.jsx' にする
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
